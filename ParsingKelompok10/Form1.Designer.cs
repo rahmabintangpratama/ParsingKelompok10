1b2198bd34dfa2a87a52fde1923e78e5b7ef0732
rahmabintangpratama <142522301+rahmabintangpratama@users.noreply.github.com>
2023-11-29 12:15:30 +0700

    Add files via upload

---
+++ ParsingKelompok10/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace ParsingKelompok10
+{
+    partial class ParsingKelompok10
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.textBoxFilePath = new System.Windows.Forms.TextBox();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.textBoxJsonContent = new System.Windows.Forms.TextBox();
+            this.textBoxOutput = new System.Windows.Forms.TextBox();
+            this.lblSourceCode = new System.Windows.Forms.Label();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(12, 46);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(130, 47);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Browse File";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // textBoxFilePath
+            // 
+            this.textBoxFilePath.Location = new System.Drawing.Point(148, 46);
+            this.textBoxFilePath.Multiline = true;
+            this.textBoxFilePath.Name = "textBoxFilePath";
+            this.textBoxFilePath.Size = new System.Drawing.Size(504, 47);
+            this.textBoxFilePath.TabIndex = 1;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(658, 46);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(130, 47);
+            this.btnCheck.TabIndex = 2;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // textBoxJsonContent
+            // 
+            this.textBoxJsonContent.Location = new System.Drawing.Point(12, 148);
+            this.textBoxJsonContent.Multiline = true;
+            this.textBoxJsonContent.Name = "textBoxJsonContent";
+            this.textBoxJsonContent.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxJsonContent.Size = new System.Drawing.Size(392, 290);
+            this.textBoxJsonContent.TabIndex = 3;
+            // 
+            // textBoxOutput
+            // 
+            this.textBoxOutput.Location = new System.Drawing.Point(410, 148);
+            this.textBoxOutput.Multiline = true;
+            this.textBoxOutput.Name = "textBoxOutput";
+            this.textBoxOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxOutput.Size = new System.Drawing.Size(378, 290);
+            this.textBoxOutput.TabIndex = 4;
+            // 
+            // lblSourceCode
+            // 
+            this.lblSourceCode.AutoSize = true;
+            this.lblSourceCode.Location = new System.Drawing.Point(13, 122);
+            this.lblSourceCode.Name = "lblSourceCode";
+            this.lblSourceCode.Size = new System.Drawing.Size(106, 20);
+            this.lblSourceCode.TabIndex = 5;
+            this.lblSourceCode.Text = "Source Code:";
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(406, 122);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(62, 20);
+            this.lblOutput.TabIndex = 6;
+            this.lblOutput.Text = "Output:";
+            // 
+            // ParsingKelompok10
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.lblSourceCode);
+            this.Controls.Add(this.textBoxOutput);
+            this.Controls.Add(this.textBoxJsonContent);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.textBoxFilePath);
+            this.Controls.Add(this.btnBrowse);
+            this.MaximizeBox = false;
+            this.Name = "ParsingKelompok10";
+            this.Text = "ParsingKelompok10";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox textBoxFilePath;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.TextBox textBoxJsonContent;
+        private System.Windows.Forms.TextBox textBoxOutput;
+        private System.Windows.Forms.Label lblSourceCode;
+        private System.Windows.Forms.Label lblOutput;
+    }
+}
+
---
+++ ParsingKelompok10/Form1.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json.Linq;
+
+namespace ParsingKelompok10
+{
+    public partial class ParsingKelompok10 : Form
+    {
+        private OpenFileDialog openFileDialog;
+
+        public ParsingKelompok10()
+        {
+            InitializeComponent();
+
+            openFileDialog = new OpenFileDialog
+            {
+                Filter = "JSON Files|*.json|All Files|*.*",
+                Title = "Pilih File JSON"
+            };
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBoxFilePath.Text = openFileDialog.FileName;
+                string jsonFilePath = openFileDialog.FileName;
+
+                // Baca isi file JSON
+                string jsonContent = File.ReadAllText(jsonFilePath);
+                textBoxJsonContent.Text = jsonContent;
+            }
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            string jsonContent = textBoxJsonContent.Text;
+
+            // Lakukan parsing JSON
+            try
+            {
+                CheckJsonCompliance(jsonContent);
+                MessageBox.Show("File JSON sesuai dengan ketentuan.", "Informasi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                textBoxOutput.Text = $"Error: {ex.Message}";
+                MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void CheckJsonCompliance(string jsonContent)
+        {
+            // Lakukan validasi sesuai dengan ketentuan
+            JObject jsonObj = JObject.Parse(jsonContent);
+
+            // Reset output text box
+            textBoxOutput.Text = "";
+
+            // Dictionary untuk menyimpan informasi state model
+            Dictionary<string, string> stateModels = new Dictionary<string, string>();
+            HashSet<string> usedKeyLetters = new HashSet<string>();
+
+            // List untuk menyimpan informasi keunikan nomor state
+            HashSet<int> stateNumbers = new HashSet<int>();
+
+            try
+            {
+                JToken modelToken = jsonObj["model"];
+                if (modelToken != null && modelToken.Type == JTokenType.Array)
+                {
+                    foreach (var model in modelToken)
+                    {
+                        string objectType = model["type"]?.ToString();
+                        string objectName = model["class_name"]?.ToString();
+
+                        // Jika object adalah class
+                        if (objectType == "class")
+                        {
+                            // Cek apakah ada assigner state model
+                            string assignerStateModelName = $"{objectName}_ASSIGNER";
+
+                            JToken assignerStateModelToken = jsonObj[assignerStateModelName];
+                            if (assignerStateModelToken == null || assignerStateModelToken.Type != JTokenType.Object)
+                            {
+                                HandleError($"Sintax error number 16: Assigner state model tidak ditemukan untuk {objectName}.");
+                                continue; // Lanjutkan ke iterasi berikutnya
+                            }
+
+                            string keyLetter = model["KL"]?.ToString();
+                            string expectedKeyLetter = $"{keyLetter}_A";
+
+                            // Cek apakah KeyLetter sesuai
+                            JToken keyLetterToken = assignerStateModelToken[objectName]?["KeyLetter"];
+                            if (keyLetterToken != null && keyLetterToken.ToString() != expectedKeyLetter)
+                            {
+                                HandleError($"Sintax error number 17: KeyLetter untuk {objectName} tidak sesuai dengan ketentuan.");
+                            }
+
+                            // Cek keunikan state model
+                            CheckUniqueness(stateModels, assignerStateModelName, $"Sintax error number 17: State model {assignerStateModelName} tidak unik.");
+
+                            // Cek keunikan state
+                            CheckStateUniqueness(stateModels, assignerStateModelToken[objectName]?["states"], objectName);
+
+                            // Cek keunikan nomor state
+                            CheckStateNumberUniqueness(stateNumbers, assignerStateModelToken[objectName]?["states"], objectName);
+
+                            // Cek keunikan KeyLetter
+                            CheckKeyLetterUniqueness(usedKeyLetters, keyLetter, objectName);
+                        }
+                    }
+                }
+
+                // Cek keunikan Timer KeyLetter
+                string timerKeyLetter = jsonObj["TIMER"]?["KeyLetter"]?.ToString();
+                CheckKeyLetterUniqueness(usedKeyLetters, timerKeyLetter, "TIMER");
+
+                // Cek event directed to one state model
+                CheckEventDirectedToStateModel(jsonObj["model"], stateModels);
+            }
+            catch (Exception ex)
+            {
+                // Tampilkan pesan kesalahan setelah semua pengecekan selesai
+                HandleError($"Error: {ex.Message}");
+            }
+        }
+
+        // Fungsi untuk menangani kesalahan dan menambahkannya ke output text box
+        private void HandleError(string errorMessage)
+        {
+            textBoxOutput.Text += $"{errorMessage}{Environment.NewLine}";
+            // throw new Exception(errorMessage); // Hapus baris ini
+        }
+
+        // Fungsi untuk memeriksa keunikan state model
+        private void CheckUniqueness(Dictionary<string, string> dictionary, string key, string errorMessage)
+        {
+            if (dictionary.ContainsKey(key))
+            {
+                HandleError(errorMessage);
+            }
+            else
+            {
+                dictionary.Add(key, key);
+            }
+        }
+
+        // Fungsi untuk memeriksa keunikan state
+        private void CheckStateUniqueness(Dictionary<string, string> stateModels, JToken statesToken, string objectName)
+        {
+            if (statesToken is JArray states)
+            {
+                foreach (var state in states)
+                {
+                    string stateName = state["state_name"]?.ToString();
+                    string stateModelName = $"{objectName}.{stateName}";
+
+                    // Cek keunikan state model
+                    CheckUniqueness(stateModels, stateModelName, $"Sintax error number 18: State {stateModelName} tidak unik.");
+                }
+            }
+        }
+
+        // Fungsi untuk memeriksa keunikan nomor state
+        private void CheckStateNumberUniqueness(HashSet<int> stateNumbers, JToken statesToken, string objectName)
+        {
+            if (statesToken is JArray stateArray)
+            {
+                foreach (var state in stateArray)
+                {
+                    int stateNumber = state["state_number"]?.ToObject<int>() ?? 0;
+
+                    if (!stateNumbers.Add(stateNumber))
+                    {
+                        HandleError($"Sintax error number 19: Nomor state {stateNumber} tidak unik.");
+                    }
+                }
+            }
+        }
+
+        // Fungsi untuk memeriksa keunikan KeyLetter
+        private void CheckKeyLetterUniqueness(HashSet<string> usedKeyLetters, string keyLetter, string objectName)
+        {
+            if (!usedKeyLetters.Add(keyLetter))
+            {
+                HandleError($"Sintax error number 20: KeyLetter untuk {objectName} tidak unik.");
+            }
+        }
+
+        // Fungsi untuk memeriksa event directed to one state model
+        private void CheckEventDirectedToStateModel(JToken modelsToken, Dictionary<string, string> stateModels)
+        {
+            if (modelsToken is JArray models)
+            {
+                foreach (var model in models)
+                {
+                    string modelName = model["class_name"]?.ToString();
+                    CheckEventDirectedToStateModelHelper(model["states"], stateModels, modelName);
+                }
+            }
+        }
+
+        private void CheckEventDirectedToStateModelHelper(JToken statesToken, Dictionary<string, string> stateModels, string modelName)
+        {
+            if (statesToken is JArray states)
+            {
+                foreach (var state in states)
+                {
+                    string stateName = state["state_name"]?.ToString();
+                    string stateModelName = $"{modelName}.{stateName}";
+
+                    if (stateModels.ContainsKey(stateModelName))
+                    {
+                        HandleError($"Sintax error number 24: State {stateModelName} tidak sesuai dengan ketentuan.");
+                    }
+                }
+            }
+        }
+    }
+}
