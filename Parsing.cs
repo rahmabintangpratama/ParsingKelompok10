0e8b7b38d5746abd0a5f6272950ecc719ab41e3c
rahmabintangpratama <142522301+rahmabintangpratama@users.noreply.github.com>
2023-12-28 16:25:10 +0700

    Add files via upload

---
+++ Parsing.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json.Linq;
+using System.Security.AccessControl;
+
+namespace Parsing
+{
+    public partial class Parsing : Form
+    {
+        private OpenFileDialog openFileDialog;
+
+        public Parsing()
+        {
+            InitializeComponent();
+
+            openFileDialog = new OpenFileDialog
+            {
+                Filter = "JSON Files|*.json|All Files|*.*",
+                Title = "Select JSON File"
+            };
+            this.Load += new EventHandler(Form1_Load);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        // Function to check uniqueness
+        private void CheckUniqueness(HashSet<string> collection, string key, string errorMessage)
+        {
+            if (collection.Contains(key))
+            {
+                HandleError(errorMessage);
+            }
+            else
+            {
+                collection.Add(key);
+            }
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBoxFilePath.Text = openFileDialog.FileName;
+                string jsonFilePath = openFileDialog.FileName;
+
+                // Read the JSON file content
+                string jsonContent = File.ReadAllText(jsonFilePath);
+                textBoxJsonContent.Text = jsonContent;
+            }
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            string jsonContent = textBoxJsonContent.Text;
+
+            // Perform JSON parsing
+            try
+            {
+                CheckJsonCompliance(jsonContent);
+            }
+            catch (Exception ex)
+            {
+                HandleError($"Error: {ex.Message}");
+                MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void HandleError(string errorMessage)
+        {
+            textBoxOutput.Text += $"{errorMessage}{Environment.NewLine}";
+            Console.WriteLine(errorMessage);
+        }
+
+        private void CheckJsonCompliance(string jsonContent)
+        {
+            try
+            {
+                JObject jsonObj = JObject.Parse(jsonContent);
+
+                // Reset output text box
+                textBoxOutput.Text = "";
+
+                // Dictionary to store state model information
+                Dictionary<string, string> stateModels = new Dictionary<string, string>();
+                HashSet<string> usedKeyLetters = new HashSet<string>();
+                HashSet<int> stateNumbers = new HashSet<int>();
+
+                JToken subsystemsToken = jsonObj["subsystems"];
+                if (subsystemsToken != null && subsystemsToken.Type == JTokenType.Array)
+                {
+                    // Iterasi untuk setiap subsystem dalam subsystemsToken
+                    foreach (var subsystem in subsystemsToken)
+                    {
+                        JToken modelToken = subsystem["model"];
+                        if (modelToken != null && modelToken.Type == JTokenType.Array)
+                        {
+                            foreach (var model in modelToken)
+                            {
+                                ValidateClassModel(model, stateModels, usedKeyLetters, stateNumbers);
+                            }
+                        }
+                    }
+
+                    // Setelah memvalidasi semua model, panggil ValidateEventDirectedToStateModelHelper untuk setiap subsystem
+                    foreach (var subsystem in subsystemsToken)
+                    {
+                        ValidateEventDirectedToStateModelHelper(subsystem["model"], stateModels, null);
+                    }
+                }
+
+                ValidateTimerModel(jsonObj, usedKeyLetters);
+            }
+            catch (Exception ex)
+            {
+                HandleError($"Error: {ex.Message}");
+                MessageBox.Show($"Error: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void ValidateClassModel(JToken model, Dictionary<string, string> stateModels, HashSet<string> usedKeyLetters, HashSet<int> stateNumbers)
+        {
+            string objectType = model["type"]?.ToString();
+            string objectName = model["class_name"]?.ToString();
+            Console.WriteLine($"Running CheckKeyLetterUniqueness for {objectName}");
+
+            if (objectType == "class")
+            {
+                Console.WriteLine($"Checking class: {objectName}");
+
+                string assignerStateModelName = $"{objectName}_ASSIGNER";
+                JToken assignerStateModelToken = model[assignerStateModelName];
+
+                if (assignerStateModelToken == null || assignerStateModelToken.Type != JTokenType.Object)
+                {
+                    HandleError($"Syntax error number 16: Assigner state model not found for {objectName}.");
+                    return;
+                }
+
+                string keyLetter = model["KL"]?.ToString();
+
+                // Pemanggilan CheckKeyLetterUniqueness
+                CheckKeyLetterUniqueness(usedKeyLetters, keyLetter, objectName);
+
+                // Check if KeyLetter is correct
+                JToken keyLetterToken = assignerStateModelToken?["KeyLetter"];
+                if (keyLetterToken != null && keyLetterToken.ToString() != keyLetter)
+                {
+                    HandleError($"Syntax error number 17: KeyLetter for {objectName} does not match the rules.");
+                }
+
+                // Check uniqueness of states
+                CheckStateUniqueness(stateModels, assignerStateModelToken?["states"], objectName, assignerStateModelName);
+
+                // Check uniqueness of state numbers
+                CheckStateNumberUniqueness(stateNumbers, assignerStateModelToken?["states"], objectName);
+
+                // Store state model information
+                string stateModelKey = $"{objectName}.{assignerStateModelName}";
+                stateModels[stateModelKey] = objectName;
+            }
+        }
+
+        private void CheckStateUniqueness(Dictionary<string, string> stateModels, JToken statesToken, string objectName, string assignerStateModelName)
+        {
+            if (statesToken is JArray states)
+            {
+                HashSet<string> uniqueStates = new HashSet<string>();
+
+                foreach (var state in states)
+                {
+                    string stateName = state["state_name"]?.ToString();
+                    string stateModelName = $"{objectName}.{stateName}";
+
+                    // Check uniqueness of state model
+                    if (!uniqueStates.Add(stateModelName))
+                    {
+                        HandleError($"Syntax error number 18: State {stateModelName} is not unique in {assignerStateModelName}.");
+                    }
+                }
+            }
+        }
+
+        private void CheckStateNumberUniqueness(HashSet<int> stateNumbers, JToken statesToken, string objectName)
+        {
+            if (statesToken is JArray stateArray)
+            {
+                foreach (var state in stateArray)
+                {
+                    int stateNumber = state["state_number"]?.ToObject<int>() ?? 0;
+
+                    if (!stateNumbers.Add(stateNumber))
+                    {
+                        HandleError($"Syntax error number 19: State number {stateNumber} is not unique.");
+                    }
+                }
+            }
+        }
+
+        private void CheckKeyLetterUniqueness(HashSet<string> usedKeyLetters, string keyLetter, string objectName)
+        {
+            string expectedKeyLetter = $"{keyLetter}_A";
+            Console.WriteLine("Running ValidateClassModel");
+            Console.WriteLine($"Checking KeyLetter uniqueness: {expectedKeyLetter} for {objectName}");
+
+            if (!usedKeyLetters.Add(expectedKeyLetter))
+            {
+                HandleError($"Syntax error number 20: KeyLetter for {objectName} is not unique.");
+            }
+        }
+
+        private void ValidateTimerModel(JObject jsonObj, HashSet<string> usedKeyLetters)
+        {
+            string timerKeyLetter = jsonObj["subsystems"]?[0]?["model"]?[0]?["KL"]?.ToString();
+            string timerStateModelName = $"{timerKeyLetter}_ASSIGNER";
+
+            JToken timerModelToken = jsonObj["subsystems"]?[0]?["model"]?[0];
+            JToken timerStateModelToken = jsonObj["subsystems"]?[0]?["model"]?[0]?[timerStateModelName];
+
+            // Check if Timer state model exists
+            if (timerStateModelToken == null || timerStateModelToken.Type != JTokenType.Object)
+            {
+                HandleError($"Syntax error number 21: Timer state model not found for TIMER.");
+                return;
+            }
+
+            // Check KeyLetter of Timer state model
+            JToken keyLetterToken = timerStateModelToken?["KeyLetter"];
+            if (keyLetterToken == null || keyLetterToken.ToString() != timerKeyLetter)
+            {
+                HandleError($"Syntax error number 21: KeyLetter for TIMER does not match the rules.");
+            }
+        }
+
+        private void ValidateEventDirectedToStateModelHelper(JToken modelsToken, Dictionary<string, string> stateModels, string modelName)
+        {
+            if (modelsToken != null && modelsToken.Type == JTokenType.Array)
+            {
+                foreach (var model in modelsToken)
+                {
+                    string modelType = model["type"]?.ToString();
+                    string className = model["class_name"]?.ToString();
+
+                    if (modelType == "class")
+                    {
+                        JToken assignerToken = model[$"{className}_ASSIGNER"];
+
+                        if (assignerToken != null)
+                        {
+                            Console.WriteLine($"assignerToken.Type: {assignerToken.Type}");
+
+                            if (assignerToken.Type == JTokenType.Object)
+                            {
+                                JToken statesToken = assignerToken["states"];
+
+                                if (statesToken != null && statesToken.Type == JTokenType.Array)
+                                {
+                                    JArray statesArray = (JArray)statesToken;
+
+                                    foreach (var stateItem in statesArray)
+                                    {
+                                        string stateName = stateItem["state_name"]?.ToString();
+                                        string stateModelName = $"{modelName}.{stateName}";
+
+                                        JToken eventsToken = stateItem["events"];
+                                        if (eventsToken is JArray events)
+                                        {
+                                            foreach (var evt in events)
+                                            {
+                                                string eventName = evt["event_name"]?.ToString();
+                                                JToken targetsToken = evt["targets"];
+
+                                                if (targetsToken is JArray targets)
+                                                {
+                                                    foreach (var target in targets)
+                                                    {
+                                                        string targetStateModel = target?.ToString();
+
+                                                        // Check if target state model is in the state models dictionary
+                                                        if (!stateModels.ContainsKey(targetStateModel))
+                                                        {
+                                                            HandleError($"Syntax error number 24: Event '{eventName}' in state '{stateModelName}' targets non-existent state model '{targetStateModel}'.");
+                                                        }
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
